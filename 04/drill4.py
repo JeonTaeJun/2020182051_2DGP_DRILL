d64fe2fa178dd5585e9256337080b3a40c84ae2d
JeonTaeJun <wjs1xo2wns3@naver.com>
2022-09-18 15:55:58 +0900

    drill #4 완성

---
+++ 04/drill4.py
@@ -0,0 +1,91 @@
+import turtle
+
+count = 0
+count2=0
+num = 200
+while(count<5):
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    count +=1
+
+count2=0
+turtle.home()
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+while(count2<5):
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    count2 +=1
+    
+count2=0
+turtle.penup()
+turtle.home()
+turtle.pendown()
+turtle.left(90)
+turtle.forward(num)
+turtle.right(90)
+num+=100
+while(count2<5):
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    count2 +=1
+
+count2=0
+turtle.penup()
+turtle.home()
+turtle.pendown()
+turtle.left(90)
+turtle.forward(num)
+turtle.right(90)
+while(count2<5):
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    count2 +=1
+
+count2=0
+turtle.penup()
+turtle.home()
+turtle.pendown()
+turtle.left(90)
+turtle.forward(num)
+turtle.right(90)
+while(count2<5):
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    count2 +=1
